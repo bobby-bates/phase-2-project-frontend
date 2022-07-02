dbae06935059b65e8f690aa9561ee08ca823dc40
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-02 10:32:11 -0400

    add results components for search and sort

---
+++ src/SearchResult.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SearchResult() {
+  return (
+    <h1>Hi from SearchResult</h1>
+  )
+}
---
+++ src/SortResults.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SortResults() {
+  return (
+    <h1>Hi from SortResults</h1>
+  )
+}
