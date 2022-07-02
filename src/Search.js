501caccf6ec446c7161f32c92fd8ef919c9bba7f
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-02 18:50:33 -0400

    add searchbox

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,24 @@
-export default function Search() {
+import { useState } from 'react'
 
+export default function Search() {
+  const [searchStr, setSearchStr] = useState('')
+
+  const handleSearchChange = e => setSearchStr(e.target.value)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(searchStr)
+
+    setSearchStr('')
+  }
   return (
-    <h1>Hi from Search</h1>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>FIND A CITY
+          <input type='text' onChange={handleSearchChange} value={searchStr} />
+        </label>
+        <button type='submit'>GO</button>
+      </form>
+    </>
   )
 }
