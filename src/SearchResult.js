f11c1d4b045a8ca7e499b91cb7bb9faaf414eadc
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-03 18:12:07 -0400

    add Google map to SearchResult

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -2,10 +2,12 @@ import { useParams, useLocation } from 'react-router-dom'
 import Table from 'react-bootstrap/Table'
 
 export default function SearchResult() {
-  // debugger
+
   // const cityParams = useParams().cityName
   const city = useLocation().state.city
-  // console.log(city)
+
+  const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${city.lat},${city.lng}&zoom=11&size=400x400&key=AIzaSyDF8PzvjuzBnixB4AGXiCfRN_2QC6GzoBk`
+
   return (
     <>
       <Table borderless>
@@ -24,6 +26,7 @@ export default function SearchResult() {
           </tr>
         </tbody>
       </Table>
+      <img src={mapUrl} alt='Google Map of Colombia' />
     </>
   )
 }
