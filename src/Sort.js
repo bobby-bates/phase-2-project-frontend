2cec58200bb8c3967ed2e067d7aaada3c4066c8b
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-03 19:57:21 -0400

    add sort dropdown button

--- src/Sort.js
+++ src/Sort.js
@@ -1,6 +1,20 @@
+import { Link, useNavigate, Outlet } from 'react-router-dom'
+import { Dropdown, DropdownButton } from 'react-bootstrap'
+
 export default function Sort() {
+  const navigate = useNavigate()
 
   return (
-    <h1>Hi from Sort</h1>
+    <>
+      <label>SORT CITIES BY
+        <DropdownButton id='dropdown-basic-button' title='SELECT'>
+          <Dropdown.Item onClick={()=>navigate('abc')}>ABC</Dropdown.Item>
+          <Dropdown.Item onClick={()=>navigate('deptartments')}>DEPARTMENTS</Dropdown.Item>
+          <Dropdown.Item onClick={()=>navigate('pop-max-min')}>POP MAX-MIN</Dropdown.Item>
+          <Dropdown.Item onClick={()=>navigate('pop-min-max')}>POP MIN-MAX</Dropdown.Item>
+        </DropdownButton>
+      </label>
+      <Outlet />
+    </>
   )
 }
