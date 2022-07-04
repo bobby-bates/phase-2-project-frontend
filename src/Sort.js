7a2b65618bee3184255bcee6a9d82915f68ed03a
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-04 13:34:02 -0400

    move logic out of switch statement and into individual functions

--- src/Sort.js
+++ src/Sort.js
@@ -4,70 +4,63 @@ import { DropdownButton, Dropdown } from 'react-bootstrap'
 export default function Sort({ cities }) {
   const navigate = useNavigate()
 
+  const sortByABC = () => {
+    // Create array of city name strings:
+    const cityNames = cities.map((city, i) => ({ i, name: city.city }))
+
+    const sortedCityNames = cityNames.sort((a, b) => {
+      const nameA = a.name
+      const nameB = b.name
+      return nameA.localeCompare(nameB, 'es-CO', { ignorePunctuation: true })
+    })
+
+    // Combine sorted city names with full cities arr:
+    const sortedCities = sortedCityNames.map(name => cities[name.i])
+
+    navigate('abc', {state:{ sortedCities: sortedCities }})
+  }
+
+  const sortByDepartments = () => {
+    // Create arr of all departments:
+    const depts = []
+    cities.forEach(city => {
+      return !depts.includes(city.adminName) ? depts.push(city.adminName) : null
+    })
+    // Loop depts and create abc sorted arr for each dept
+    let deptArrs = []
+    depts.forEach(dept => {
+      const deptArr = cities.filter(city => city.adminName === dept)
+        deptArr.sort((a, b) => {
+        const cityA = a.city
+        const cityB = b.city
+        return cityA.localeCompare(cityB, 'es-CO', { ignorePunctuation: true })
+      })
+      deptArrs.push(deptArr)
+    })
+    // Merge all sorted dept arrs into single arr:
+    const citiesByDeptSingleArr = deptArrs.flat()
+    navigate('departments',
+      {state:{
+        citiesByDeptSingleArr: citiesByDeptSingleArr,
+        citiesByDeptManyArrs: deptArrs
+      }})
+  }
   const handleSort = e => {
-    // debugger
     const name = e.target.name
-
-
-    // debugger
     // eslint-disable-next-line default-case
     switch (name) {
-      case 'abc': {
-          // Create array of city name strings:
-          // const cityNames = () => cities.map(city => city.city)
-          const cityNames = cities.map((city, i) => ({ i, name: city.city }))
-          // debugger
-          const sortedCityNames = cityNames.sort((a, b) => {
-            // debugger
-            const nameA = a.name
-            const nameB = b.name
-
-            return nameA.localeCompare(nameB, 'es-CO', { ignorePunctuation: true })
-        })
-        // debugger
-        // Combine sorted city names with full cities arr:
-        const sortedCities = sortedCityNames.map(name => cities[name.i])
-        // debugger
-        navigate('abc', {state:{ sortedCities: sortedCities }})
+      case 'abc':
+        sortByABC()
         break
-      }
-      case 'departments': {
-        // Create arr of all departments:
-        const depts = []
-        cities.forEach(city => {
-          return !depts.includes(city.adminName) ? depts.push(city.adminName) : null
-        })
-
-        // Loop depts and create abc sorted arr for each dept
-        let deptArrs = []
-        depts.forEach(dept => {
-          const deptArr = cities.filter(city => city.adminName === dept)
-            deptArr.sort((a, b) => {
-            const cityA = a.city
-            const cityB = b.city
-            return cityA.localeCompare(cityB, 'es-CO', { ignorePunctuation: true })
-          })
-          deptArrs.push(deptArr)
-        })
-
-        // Merge all sorted dept arrs into single arr:
-        const citiesByDeptSingleArr = deptArrs.flat()
-
-        navigate('departments',
-          {state:{
-            citiesByDeptSingleArr: citiesByDeptSingleArr,
-            citiesByDeptManyArrs: deptArrs
-          }})
+      case 'departments':
+        sortByDepartments()
         break
-      }
-      case 'pop-max-min': {
+      case 'pop-max-min':
         navigate('pop-max-min')
         break
-      }
-      case 'pop-min-max': {
+      case 'pop-min-max':
         navigate('pop-min-max')
         break
-      }
     }
   }
 
