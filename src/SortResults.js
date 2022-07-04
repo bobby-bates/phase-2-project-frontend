517e6e188d0ab459545226c2b3ee922177bc36d5
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-04 15:46:30 -0400

    refactor return JSX

--- src/SortResults.js
+++ src/SortResults.js
@@ -5,13 +5,12 @@ export default function SortResults() {
   const location = useLocation()
 
   const route = location.pathname
-  let sortOutput
+  let title
+  let tableOutput
 
   const abcOutput = () => {
     const sortedCities = location.state.sortedCities
-    console.log(sortedCities)
-    const cityList = sortedCities.map(city => {
-      // debugger
+    return sortedCities.map(city => {
       return (
         <tr key={`${city.city}${city.population}`}>
           <td>{city.city}</td>
@@ -20,22 +19,6 @@ export default function SortResults() {
         </tr>
       )
     })
-
-    return (
-      <>
-        <h2>ALPHABETICAL ORDER:</h2>
-        <Table borderless>
-          <tbody>
-            <tr>
-              <th>City:</th>
-              <th>Department:</th>
-              <th>Population:</th>
-            </tr>
-            {cityList}
-          </tbody>
-        </Table>
-      </>
-    )
   }
 
   const deptOutput = () => {
@@ -49,20 +32,33 @@ export default function SortResults() {
 
   switch (route) {
     case '/sort/abc':
-      sortOutput = abcOutput()
+      title = 'ALPHABETICAL ORDER:'
+      tableOutput = abcOutput()
       break
     case '/sort/departments':
-      sortOutput = deptOutput()
+      tableOutput = deptOutput()
       break
     case '/sort/pop-max-min':
     case '/sort/pop-min-max':
-      sortOutput = popOutput(route)
+      tableOutput = popOutput(route)
       break
     default:
       console.log('Switch error!')
   }
 
   return (
-    <>{sortOutput}</>
+    <>
+      <h2>{title}</h2>
+        <Table borderless>
+          <tbody>
+            <tr>
+              <th>City:</th>
+              <th>Department:</th>
+              <th>Population:</th>
+            </tr>
+            {tableOutput}
+          </tbody>
+        </Table>
+    </>
   )
 }
