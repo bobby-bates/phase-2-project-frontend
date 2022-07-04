3157c819c701ae0d462f6ab22ff048f52a5f212c
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-04 19:34:42 -0400

    complete fetch POST

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Alert from 'react-bootstrap/Alert'
 import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './NavBar'
@@ -12,7 +11,6 @@ import SortResults from './SortResults'
 export default function App() {
   const [cities, setCities] = useState([])
   const [alert, setAlert] = useState()
-
 
   useEffect(() => {
     fetch('http://localhost:3001/colombiaCities')
@@ -27,58 +25,63 @@ export default function App() {
       const name = e.target.innerText
       const dept = e.target.dataset.cityDept
       const foundCity = cities.find(city => city.city === name && city.adminName === dept)
-      console.log(cities[1].fav)
+      // console.log(cities[1].fav)
       if (!foundCity.fav) {
         setCities([...cities, foundCity.fav = true])
-        setAlert(
-          <Alert key={'primary'} variant={'primary'}>
-            {name} added as a favorite!
-          </Alert>
-        )
+        setAlert(<h1>{name} added as a favorite!</h1>)
       } else {
         setCities([...cities, foundCity.fav = false])
+        setAlert(<h1>{name} removed from favorites!</h1>)
       }
-      // console.log('after:', cities[1])
-      // debugger
 
       // Back end:
-
+      fetch('http://localhost:3001/favoriteCities', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ city: name }),
+      })
+      .catch(e => console.error('FETCH POST ERROR:', e))
     }
 
   return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path='/' element={<NavBar />}>
-        <Route path='search' element={<Search cities={cities} alert={alert}/>}>
-          <Route 
-            path=':cityName'
-            element={<SearchResult
-            handleCityClick={handleCityClick} />}
-          />
+    <>
+      {alert}
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/' element={<NavBar />}>
+          <Route path='search' element={<Search cities={cities} />}>
+            <Route 
+              path=':cityName'
+              element={<SearchResult
+              handleCityClick={handleCityClick} />}
+            />
+          </Route>
+          <Route path='sort' element={<Sort cities={cities} />}>
+            <Route
+              path='abc'
+              element={<SortResults
+              handleCityClick={handleCityClick} />}
+            />
+            <Route
+              path='departments'
+              element={<SortResults
+              handleCityClick={handleCityClick} />}
+            />
+            <Route
+              path='pop-max-min'
+              element={<SortResults
+              handleCityClick={handleCityClick} />}
+            />
+            <Route
+              path='pop-min-max'
+              element={<SortResults
+              handleCityClick={handleCityClick} />}
+            />
+          </Route>
         </Route>
-        <Route path='sort' element={<Sort cities={cities} />}>
-          <Route
-            path='abc'
-            element={<SortResults
-            handleCityClick={handleCityClick} />}
-          />
-          <Route
-            path='departments'
-            element={<SortResults
-            handleCityClick={handleCityClick} />}
-          />
-          <Route
-            path='pop-max-min'
-            element={<SortResults
-            handleCityClick={handleCityClick} />}
-          />
-          <Route
-            path='pop-min-max'
-            element={<SortResults
-            handleCityClick={handleCityClick} />}
-          />
-        </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </>
   )
 }
