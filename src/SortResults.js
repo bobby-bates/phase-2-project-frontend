36dc86b33c964e3b536016fe07d38e8a2869ca53
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-04 15:37:02 -0400

    display abc sort result text

--- src/SortResults.js
+++ src/SortResults.js
@@ -1,39 +1,68 @@
 import { useLocation } from 'react-router-dom'
-import React from 'react'
+import Table from 'react-bootstrap/Table'
 
 export default function SortResults() {
   const location = useLocation()
-  // debugger
-
-  // if (location.pathname === '/sort/pop-max-min') {
-    
-  // } else {
-    
-  // }
 
   const route = location.pathname
+  let sortOutput
+
+  const abcOutput = () => {
+    const sortedCities = location.state.sortedCities
+    console.log(sortedCities)
+    const cityList = sortedCities.map(city => {
+      // debugger
+      return (
+        <tr key={`${city.city}${city.population}`}>
+          <td>{city.city}</td>
+          <td>{city.adminName}</td>
+          <td>{city.population}</td>
+        </tr>
+      )
+    })
+
+    return (
+      <>
+        <h2>ALPHABETICAL ORDER:</h2>
+        <Table borderless>
+          <tbody>
+            <tr>
+              <th>City:</th>
+              <th>Department:</th>
+              <th>Population:</th>
+            </tr>
+            {cityList}
+          </tbody>
+        </Table>
+      </>
+    )
+  }
+
+  const deptOutput = () => {
+    const citiesByDeptSingleArr = location.state.citiesByDeptSingleArr
+    const citiesByDeptManyArrs = location.state.citiesByDeptManyArrs
+  }
+
+  const popOutput = route => {
+    const citiesByPop = location.state.citiesByPop
+  }
 
   switch (route) {
     case '/sort/abc':
-      console.log('abc cities from SortResults:', location.state.sortedCities)
+      sortOutput = abcOutput()
       break
     case '/sort/departments':
-      console.log('cities by dept (single arr) from SortResults:', location.state.citiesByDeptSingleArr)
-      console.log('cities by dept (many arrs) from SortResults:', location.state.citiesByDeptManyArrs)
+      sortOutput = deptOutput()
       break
     case '/sort/pop-max-min':
-      console.log('cities by pop max-min from SortResults:', location.state.citiesByPop)
-      break
     case '/sort/pop-min-max':
-      console.log('cities by pop min-max from SortResults:', location.state.citiesByPop)
+      sortOutput = popOutput(route)
       break
     default:
       console.log('Switch error!')
   }
 
-
-
   return (
-    <h1>Hi from SortResults</h1>
+    <>{sortOutput}</>
   )
 }
